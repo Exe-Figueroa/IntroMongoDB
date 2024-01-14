fdbb9da133f7542a0c2ab88ec61e33df50a3e92f
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2024-01-14 18:36:13 -0300

    06-Mongo en VSCode

---
+++ NotasCurso.js
@@ -0,0 +1,10 @@
+/** 06-Mongo en VSCode
+ * *Primero se instala la extension oficial de mongo db
+ * *Conectarse al cluster creado en mongo atlas
+ * *Cambiar el nombre (opcional)
+ * *Crear carpeta para guardar los archivos con las consultas
+ * *use => es para identificar a que db nos vamos a conectar
+ * *db.{collection} => es para seleccionar una colección específica dentro de la db seleccionada
+ * *.find() => Es para filtrar en base a alguna propiedad específica
+ * *.count()=> Nos dice la cantidad de elementos que coinciden con la característica especificada
+ */
